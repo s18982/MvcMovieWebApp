b3fe0e9688afab4fd2a823b2995763dcc1819d34
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-20 22:06:46 +0200

    Dodanie kontrolera

---
+++ MvcMovieWebApplication/Controllers/HomeWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace MvcMovieWebApplication.Controllers
+{
+    public class HomeWorldController : Controller
+    {       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
--- MvcMovieWebApplication/Startup.cs
+++ MvcMovieWebApplication/Startup.cs
@@ -50,7 +50,7 @@ namespace MvcMovieWebApplication
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=HomeWorld}/{action=Index}/{id?}");
             });
         }
     }
