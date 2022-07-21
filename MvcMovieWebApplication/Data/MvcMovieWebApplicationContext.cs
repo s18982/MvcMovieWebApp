4d5adea97b663b04a607574df687fd73d8dcf6ab
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-07-21 16:56:46 +0200

    Tworzenie szkieletu z uzyciem Entity FC

---
+++ MvcMovieWebApplication/Data/MvcMovieWebApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieWebApplication.Models;
+
+namespace MvcMovieWebApplication.Data
+{
+    public class MvcMovieWebApplicationContext : DbContext
+    {
+        public MvcMovieWebApplicationContext (DbContextOptions<MvcMovieWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieWebApplication.Models.Movie> Movie { get; set; }
+    }
+}
--- MvcMovieWebApplication/Startup.cs
+++ MvcMovieWebApplication/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieWebApplication.Data;
 
 namespace MvcMovieWebApplication
 {
@@ -24,6 +26,9 @@ namespace MvcMovieWebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieWebApplicationContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieWebApplicationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
